2a25d523fb84885b25acf459cfacedc0e6926391
Daniel Lima <danielmlimag@gmail.com>
2025-01-03 23:41:38 +0100

    fix: show photo properly

--- frontend/src/app/homepage/photo-modal/photo-modal.component.ts
+++ frontend/src/app/homepage/photo-modal/photo-modal.component.ts
@@ -9,6 +9,8 @@ import {
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import KeenSlider, { KeenSliderInstance } from 'keen-slider';
 import { CommonModule } from '@angular/common';
+import { PhotoService } from '../../services/photo.service';
+import { Subscription } from 'rxjs';
 
 export interface PhotoModalData {
   photos: { id: string; createdAt: string; url: string }[];
@@ -24,35 +26,56 @@ export interface PhotoModalData {
 })
 export class PhotoModalComponent implements OnInit, OnDestroy, AfterViewInit {
   slider: KeenSliderInstance | null = null;
+  photos: { id: string; createdAt: string; url: string }[] = [];
+  private photosSubscription: Subscription | null = null;
 
   constructor(
     public dialogRef: MatDialogRef<PhotoModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: PhotoModalData,
-    private elementRef: ElementRef
+    private elementRef: ElementRef,
+    private photoService: PhotoService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.photosSubscription = this.photoService.photos$.subscribe((photos) => {
+      this.photos = photos.map((photo) => ({
+        id: photo.id,
+        createdAt: photo.createdAt.toString(),
+        url: photo.image.toString(),
+      }));
+      this.initSlider();
+    });
+  }
 
   ngAfterViewInit(): void {
+    this.initSlider();
+  }
+
+  private initSlider(): void {
     const sliderContainer =
       this.elementRef.nativeElement.querySelector('.keen-slider');
-    this.slider = new KeenSlider(sliderContainer, {
-      loop: true,
-      slides: { perView: 1 },
-      breakpoints: {
-        '(min-width: 768px)': {
-          slides: { perView: 2 },
+    if (sliderContainer) {
+      this.slider = new KeenSlider(sliderContainer, {
+        loop: false,
+        slides: { perView: 1 },
+        breakpoints: {
+          '(min-width: 768px)': {
+            slides: { perView: 2 },
+          },
+          '(min-width: 1200px)': {
+            slides: { perView: 3 },
+          },
         },
-        '(min-width: 1200px)': {
-          slides: { perView: 3 },
-        },
-      },
-    });
+      });
+    }
   }
 
   ngOnDestroy(): void {
     if (this.slider) {
       this.slider.destroy();
+    }
+    if (this.photosSubscription) {
+      this.photosSubscription.unsubscribe();
     }
   }
 
