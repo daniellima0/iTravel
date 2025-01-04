144d29865e847948bd3e2767f6e3dee82b04a75f
Daniel Lima <danielmlimag@gmail.com>
2025-01-04 20:12:15 +0100

    feat: add auth check to Homepage

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar.component';
 import { Map } from './map/map.component';
 import { UploadPhotoButton } from './upload-photo-button/upload-photo-button.component';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -10,4 +12,20 @@ import { UploadPhotoButton } from './upload-photo-button/upload-photo-button.com
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
-export class Homepage {}
+export class Homepage {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:3000/auth/status', { withCredentials: true })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (err) => {
+          console.log('Not authenticated');
+          this.router.navigate(['login']);
+        },
+      });
+  }
+}
